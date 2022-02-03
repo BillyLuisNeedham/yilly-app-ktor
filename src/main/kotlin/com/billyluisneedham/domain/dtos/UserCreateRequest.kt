b345b8b715dfebfd417ba0f27f3785c4b7884bf0
BillyLuisNeedham <billyluisneedham@gmail.com>
2022-02-03 19:40:16 +0000

    UserCreateRequest.kt serail names added

--- src/main/kotlin/com/billyluisneedham/domain/dtos/UserCreateRequest.kt
+++ src/main/kotlin/com/billyluisneedham/domain/dtos/UserCreateRequest.kt
@@ -1,10 +1,11 @@
 package com.billyluisneedham.domain.dtos
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class UserCreateRequest(
-    val firstName: String,
-    val lastName: String,
+    @SerialName("first_name") val firstName: String,
+    @SerialName("last_name") val lastName: String,
     val email: String,
 )
